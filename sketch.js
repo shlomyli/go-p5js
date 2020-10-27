0f4cd2ec775c8be0d514e559bb16c821aa865441
shlomyli <shlomyli@users.noreply.github.com>
2020-10-27 19:44:09 +0200

    Create sketch.js
    Initial version

---
+++ sketch.js
@@ -0,0 +1,38 @@
+// Daniel Shiffman
+// http://codingtra.in
+// Steering Text Paths
+// Video: https://www.youtube.com/watch?v=4hA7G3gup-4
+
+let font;
+let vehicles = [];
+
+function preload() {
+    font = loadFont('./Heebo[wght].ttf');
+}
+
+function setup() {
+    createCanvas(1200, 800);
+    background(51);
+
+    var points = font.textToPoints('ךֵל', 400, 400, 400, {
+        sampleFactor: 0.5
+    });
+
+    for (var i = 0; i < points.length; i++) {
+        var pt = points[i];
+        var vehicle = new Vehicle(pt.x, pt.y);
+        vehicles.push(vehicle);
+    }
+}
+
+function draw() {
+
+    background(51);
+
+    for (var i = 0; i < vehicles.length; i++) {
+        var v = vehicles[i];
+        v.behaviors();
+        v.update();
+        v.show();
+    }
+}
